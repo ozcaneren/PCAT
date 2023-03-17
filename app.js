528a6af6b197ba58c08cf1e259fe6a8e1b56cc58
Eren Ozcan <erenozcandev@gmail.com>
2023-03-17 18:12:34 +0300

    add static files

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+const myLogger = (req, res, next) => {
+  console.log('LOGGED');
+  next();
+};
+
+app.use(express.static('public'));
+app.use(myLogger);
+
 app.get('/', (req, res) => {
-  res.send('Hi');
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
 
 const port = 3000;
