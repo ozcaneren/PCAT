f9dc5f0b70de4bcbb7885cfdcf7076ed9f411043
Eren Ozcan <erenozcandev@gmail.com>
2023-03-21 06:03:26 +0300

    add ejs

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
+
+// EJS
+app.set('view engine', 'ejs');
+
 
 const myLogger = (req, res, next) => {
   console.log('LOGGED');
   next();
 };
 
+// Static Files
 app.use(express.static('public'));
 app.use(myLogger);
 
+// Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
 });
 
 const port = 3000;
