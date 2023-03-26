4018449adf29c69adaf8c2e969bb2e0f631d81fc
Eren Ozcan <erenozcandev@gmail.com>
2023-03-26 23:16:21 +0300

    connect db

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const ejs = require('ejs');
+const Photo = require('./models/Photo');
 
 const app = express();
+
+//connect DB
+mongoose.connect('mongodb://127.0.0.1:27017/pcat', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // EJS
 app.set('view engine', 'ejs');
 
-
-const myLogger = (req, res, next) => {
-  console.log('LOGGED');
-  next();
-};
-
 // Static Files
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({})
+  res.render('index', {
+    photos
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -30,8 +36,9 @@ app.get('/add', (req, res) => {
   res.render('add');
 });
 
-app.post('/photos', (req, res) => {
-  res.render('add');
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body)
+  res.redirect('/')
 });
 
 const port = 3000;
