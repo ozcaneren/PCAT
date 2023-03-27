69a18d2041d2eeba5592f04f4dac30ef1ca06da0
Eren Ozcan <erenozcandev@gmail.com>
2023-03-27 16:10:14 +0300

    delete photo

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
 const path = require('path');
 const ejs = require('ejs');
 const fs = require('fs');
 const Photo = require('./models/Photo');
 
 const app = express();
-
 
 //connect DB
 mongoose.connect('mongodb://127.0.0.1:27017/pcat', {
@@ -23,6 +23,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
+app.use(methodOverride('_method', { methods: ['POST', 'GET'] }));
 
 // Routes
 app.get('/', async (req, res) => {
@@ -75,6 +76,23 @@ app.get('/photos/edit/:id', async (req, res) => {
   res.render('edit', {
     photo,
   });
+});
+
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title;
+  photo.description = req.body.description;
+  photo.save();
+
+  res.redirect(`/photos/${req.params.id}`);
+});
+
+app.delete('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  let deletedImage = __dirname + '/public' + photo.image;
+  fs.unlinkSync(deletedImage);
+  await Photo.findByIdAndRemove(req.params.id);
+  res.redirect('/');
 });
 
 const port = 3000;
